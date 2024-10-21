0c653747578b877e08522139c74c91a0097fda15
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-10-21 03:52:14 +0300

    Verify hashed password

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,3 +4,8 @@ import * as argon2 from "argon2";
 export const hashPassword = async (password: string): Promise<string> => {
   return await argon2.hash(password);
 };
+
+// verify hashed password
+export const verifyPassword = async (hashedPassword: string, password: string): Promise<boolean> => {
+  return await argon2.verify(hashedPassword, password);
+};
