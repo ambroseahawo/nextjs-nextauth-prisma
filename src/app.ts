2fcbeb2cfaee474a6713a658415dee0123fb890a
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-10-21 04:46:21 +0300

    Set app entry file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
-import express, { Request, Response } from 'express';
+import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
+import { corsHandler } from "./middlewares/cors";
+import routes from "./routes";
 
+const PORT = 4000;
 const app = express();
-const PORT = 3000;
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello, World!');
-});
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "10mb" }));
+
+app.use(helmet());
+// CORS middleware with credentials support
+app.use(corsHandler);
+
+// Explicitly handle preflight OPTIONS requests
+// app.options('*', cors());
+
+app.use("/api/auth", routes.authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
