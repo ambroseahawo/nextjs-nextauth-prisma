bc38588f619b749bb55c44f956c0f09527b3e40d
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-11-01 00:46:14 +0300

    Create new user

--- src/actions/register.tsx
+++ src/actions/register.tsx
@@ -1,7 +1,10 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,5 +12,16 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   if (!validatedFields.success) return { error: "Invalid fields" };
 
-  return { success: "Email sent" };
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) return { error: "Email already registered" };
+
+  // create user
+  await db.user.create({ data: { name, email, password: hashedPassword } });
+
+  // send verification token
+
+  return { success: "User created" };
 };
