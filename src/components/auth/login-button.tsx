a71630c379a7ccd220e3cd470a245e0f7a9565f3
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-10-31 19:15:30 +0300

    Implement auth components

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
 
 interface LoginButtonProps {
   children: React.ReactNode;
-  mode?: 'modal' | 'redirect';
+  mode?: "modal" | "redirect";
   asChild?: boolean;
 }
 
-export const LoginButton = ({children, mode='redirect', asChild}: LoginButtonProps) => {
-  const router = useRouter()
+export const LoginButton = ({ children, mode = "redirect", asChild }: LoginButtonProps) => {
+  const router = useRouter();
 
-  const onClick = () =>{
-    router.push('/auth/login')
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode == "modal") {
+    return <span>TODO: Implement modal</span>;
   }
 
-  if (mode == 'modal'){
-    return(
-      <span>TODO: Implement modal</span>
-    )
-  }
-
-  return(
-    <span onClick={onClick} className="cursor-pointer">{children}</span>
-  )
-}
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { LoginSchema } from "@/schemas";
+
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <CardWrapper
+      headerLabel="Welcome Back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="bob@gmail.com" type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="********" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
