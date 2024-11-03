52e6f1b745e07f836630f23390b2de80859c976b
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-11-03 23:22:15 +0300

    Implement auth middleware

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An array of routes that are accessible to public
+ * These routes don't need authentication
+ * @type {string[]}
+ */
+export const publicRoutes: string[] = ["/"];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged in users to protected routes
+ * @type {string[]}
+ */
+export const authRoutes: string[] = ["/auth/register", "/auth/login"];
+
+/**
+ * Prefix for api authentication routes
+ * Routes that start with this prefix are used for api authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix: string = "/api/auth";
+
+/**
+ * The default redirect route after login
+ *@type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = "/settings";
