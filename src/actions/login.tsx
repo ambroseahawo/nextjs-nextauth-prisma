96726f9a64295d2549ba5788f4dccb369071a79c
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-11-04 07:25:29 +0300

    Fix nextauth login error

--- src/actions/login.tsx
+++ src/actions/login.tsx
@@ -20,6 +20,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
+
+    return { success: "Login successful" };
   } catch (error) {
     console.log("LOGIN ERROR: ", error);
 
