503ba4b81c6f3106c1ffee6b55ea350087d64089
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-11-05 06:23:04 +0300

    Implement social auth

--- src/auth.ts
+++ src/auth.ts
@@ -20,6 +20,14 @@ export const {
   signOut,
   handlers: { GET, POST },
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     async session({ token, session }) {
       // session.user.customField = "value";
