84375f7d84dc18fb8e9a5eced4103e0b1f434b7d
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-11-07 01:27:39 +0300

    Implement admin

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -3,11 +3,12 @@
 import { userRole } from "@prisma/client";
 import { toast } from "sonner";
 
-import { admin } from "@/actions/admin";
 import { RoleGate } from "@/components/auth/role-gate";
 import { FormSuccess } from "@/components/form-success";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+import { admin } from "@/actions/admin";
 
 const AdminPage = () => {
   const onAPIRouteClick = () => {
